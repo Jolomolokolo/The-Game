6b56bcdecf909b965c41005dfcc02d390101158a
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2026-06-08 01:08:27 +0200

    Add car scene and assets; update player & project
    Add a new car scene (Scenes/car.tscn) including mesh, material, large vertex data and a car.gd script (plus UID). Add Scenes/cone.tscn and temporary scene copies. Introduce car.gd and supporting files. Update Scenes/player.tscn and Scripts/player.gd, modify main.tscn and project.godot, and update character assets (characterMedium FBX + .import, run.fbx.import) and an animation resource. Temporary .tmp files were also added during the import/export process.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -19,6 +19,9 @@ var fall_velocity := 0.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var camera_selected = 2
 
+var in_car := false
+var nearby_car : Node = null
+
 # Smooth rotation system
 var cam_pitch := 0.0
 var target_yaw := 0.0
@@ -29,30 +32,30 @@ var current_yaw := 0.0
 @onready var camera_first_view = $"CameraPivot/First-View"
 @onready var camera_third_view = $"CameraPivot/SpringArm3D/Third-View"
 #@onready var head_mesh = $"Skeleton3D/head-mesh"
+@onready var tooltip_label = $"CanvasLayer/Tooltip-Overlay"
 
 # Animation
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready():
+	add_to_group("player")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	camera_first_view.current = false
 	camera_third_view.current = true
 	#head_mesh.visible = true
 	health = max_health
 	call_deferred("_setup_collision_exceptions")
+	tooltip_label.visible = false
 	
 func _input(event):
 	
-	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and not in_car:
 
 		target_yaw += deg_to_rad(-event.relative.x * mouse_sensitivity)
 
 		cam_pitch += deg_to_rad(-event.relative.y * mouse_sensitivity)
 		cam_pitch = clamp(cam_pitch, deg_to_rad(-50), deg_to_rad(50))
 
-	if event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
@@ -65,8 +68,17 @@ func _input(event):
 		camera_selected = 1
 	if event.is_action_pressed("camera_2"):
 		camera_selected = 2
-
+	
+	if event.is_action_pressed("ui_interact") and nearby_car != null:
+		if not in_car:
+			in_car = true
+			set_process_unhandled_input(false)
+			nearby_car.enter_car(self)
+	
 func _physics_process(delta):
+	
+	if in_car:
+		return
 	
 	# Smooth body rotation
 	camera_pivot.rotation.x = cam_pitch
@@ -101,8 +113,7 @@ func _physics_process(delta):
 		"ui_left",
 		"ui_right",
 		"ui_up",
-		"ui_down"
-	)
+		"ui_down")
 	
 	# Movement direction
 	var direction = (
@@ -127,24 +138,33 @@ func _physics_process(delta):
 		if is_on_floor():
 			if current_speed == sprint_speed:
 				if animation_player.current_animation != "anmimationes/Root_Run" and animation_player.current_animation != "anmimationes/Root_Jump":
+					animation_player.speed_scale = 1.0
 					animation_player.play("anmimationes/Root_Run")
 			else:
 				if animation_player.current_animation != "anmimationes/Root_Run" and animation_player.current_animation != "anmimationes/Root_Jump":
+					animation_player.speed_scale = 0.6
 					animation_player.play("anmimationes/Root_Run")
-					
+	
 	elif is_on_floor() and not Input.is_action_just_pressed("ui_accept"):
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
 	
 		if is_on_floor():
 			if animation_player.current_animation != "anmimationes/Root_Idle" and animation_player.current_animation != "anmimationes/Root_Jump":
+				animation_player.speed_scale = 0.6
 				animation_player.play("anmimationes/Root_Idle")
 	
 	move_and_slide()
 	
 func _process(_delta):
 	
-	#Camera Controll/Retraction
+	if in_car:
+		tooltip_label.visible = false
+		return
+	
+	tooltip_label.visible = nearby_car != null
+	
+	# Camera Controll/Retraction
 	var dist = global_position.distance_to(camera_third_view.global_position)
 	
 	if dist < 2.0:
@@ -183,3 +203,8 @@ func _setup_collision_exceptions():
 	$Root/Skeleton3D/PhysicalBoneSimulator3D.physical_bones_add_collision_exception(
 		self.get_rid()
 	)
+	
+func notify_exit():
+	in_car = false
+	nearby_car = null
+	set_process_unhandled_input(true)
---
+++ car.gd
@@ -0,0 +1,105 @@
+extends VehicleBody3D
+
+@export var max_engine_force := 1500.0
+@export var max_brake_force := 80.0
+@export var max_steering := 0.4
+@export var reverse_force := 800.0
+@export var enter_distance := 3.0
+
+var player_inside := false
+var player_ref : Node = null
+var just_entered := false
+
+@onready var car_camera_first_view = $"First-View"
+@onready var car_camera_third_view = $"Third-View"
+@onready var reverse_viewport_container = $SubViewportContainer
+@onready var reverse_camera = $SubViewportContainer/SubViewport/Camera3D
+
+func _ready():
+	add_to_group("car")
+	car_camera_first_view.current = false
+	reverse_viewport_container.visible = false
+	
+func _physics_process(delta):
+	if not player_inside:
+		return
+	
+	if just_entered:
+		just_entered = false
+		return
+	
+	if Input.is_action_just_pressed("ui_interact"):
+		exit_car()
+		return
+	
+	if Input.is_action_just_pressed("camera_1"):
+		car_camera_first_view.current = true
+		car_camera_third_view.current = false
+	if Input.is_action_just_pressed("camera_2"):
+		car_camera_first_view.current = false
+		car_camera_third_view.current = true
+	
+	var forward = Input.get_action_strength("ui_up")
+	var back = Input.get_action_strength("ui_down")
+	
+	var speed = linear_velocity.dot(-global_transform.basis.z)
+	if back > 0 and speed > 0.1: # Adden, das etwas überblende Zeit oder so
+		reverse_viewport_container.visible = true
+	else:
+		reverse_viewport_container.visible = false
+	
+	if back > 0 and speed > -0.5:
+		engine_force = -reverse_force * back
+		brake = 0.0
+	elif back > 0:
+		engine_force = 0.0
+		brake = max_brake_force * back
+	elif forward > 0:
+		engine_force = max_engine_force * forward
+		brake = 0.0
+	else:
+		engine_force = 0.0
+		brake = 10.0
+		
+	var steer_target = Input.get_axis("ui_right", "ui_left") * max_steering
+	steering = lerp(steering, steer_target, delta * 8.0)
+	
+func enter_car(player):
+	player_ref = player
+	player_inside = true
+	just_entered = true
+	player_ref.hide()
+	player_ref.set_physics_process(false)
+	player_ref.set_collision_layer_value(1, false)
+	player_ref.set_collision_mask_value(1, false)
+	car_camera_first_view.current = true
+	brake = 0.0
+	
+func exit_car():
+	if player_ref == null:
+		return
+	player_inside = false
+	car_camera_first_view.current = false
+	engine_force = 0.0
+	brake = max_brake_force
+	steering = 0.0
+	player_ref.global_position = global_position + global_transform.basis.x * 2.0
+	player_ref.show()
+	player_ref.set_physics_process(true)
+	player_ref.notify_exit()
+	var ref = player_ref
+	player_ref = null
+	await get_tree().create_timer(0.05).timeout
+	if is_instance_valid(ref):
+		ref.set_collision_layer_value(1, true)
+		ref.set_collision_mask_value(1, true)
+
+func _on_enter_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		player_ref = body
+		body.nearby_car = self
+	
+func _on_enter_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player") and not player_inside:
+		player_ref = null
+		body.nearby_car = null
