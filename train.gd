1ab98735390d8037932bbd329f3df9b57f9dcc5c
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2026-06-19 09:21:05 +0200

    Added complete lights to the train

--- train.gd
+++ train.gd
@@ -8,20 +8,44 @@ var speed := 0.0
 var player_inside := false
 var player_ref : Node = null
 var just_entered := false
+var headlights_on := false
+var gear := 0
+var gear_names := {-1: "Reverse", 0: "Neutral", 1: "Forward"}
 
 @onready var path_follow : PathFollow3D = get_parent()
 @onready var train_camera = $DriveCamera
 @onready var collision_body = $StaticBody3D
 
+@onready var spot_front_right = $"SpotLight - front - right"
+@onready var spot_front_left = $"SpotLight - front - left"
+@onready var spot_back_right = $"SpotLight - back - right"
+@onready var spot_back_left = $"SpotLight - back - left"
+@onready var back_front_right = $"BackLight - front - right"
+@onready var back_front_left = $"BackLight - front - left"
+@onready var back_back_right = $"BackLight - back - right"
+@onready var back_back_left = $"BackLight - back - left"
+
+var headlights : Array
+var brake_lights : Array
+
 func _ready():
 	add_to_group("car")
 	train_camera.current = false
+	spot_front_right.visible = false
+	spot_front_left.visible = false
+	spot_back_right.visible = false
+	spot_back_left.visible = false
+	back_front_right.visible = false
+	back_front_left.visible = false
+	back_back_right.visible = false
+	back_back_left.visible = false
 	
 func _physics_process(delta):
 	collision_body.global_position = global_position
 	collision_body.global_rotation = global_rotation
 	
 	if not player_inside:
+		_update_headlights()
 		return
 	
 	if just_entered:
@@ -32,26 +56,61 @@ func _physics_process(delta):
 		exit_train()
 		return
 	
+	if Input.is_action_just_pressed("train_up") and abs(speed) < 1.0:
+		gear = min(gear + 1, 1)
+	
+	if Input.is_action_just_pressed("train_down") and abs(speed) < 1.0:
+		gear = max(gear -1, -1)
+	
 	var forward = Input.get_action_strength("ui_up")
 	var back = Input.get_action_strength("ui_down")
 	
-	if forward > 0:
-		speed = move_toward(speed, max_speed, acceleration * delta)
+	if gear == 0:
+		speed = move_toward(speed, 0.0, brake_force * delta)
+	elif forward > 0:
+		speed = move_toward(speed, max_speed * gear, acceleration * delta)
 	elif back > 0:
-		if speed > 0.5:
-			speed = move_toward(speed, 0.0, brake_force * delta)
-		else:
-			speed = move_toward(speed, -max_speed * 0.3, acceleration * delta)
-		
+		speed = move_toward(speed, 0.0, brake_force * delta)
 	else:
-		speed = move_toward(speed, 0.0, brake_force * 3.0 * delta)
+		speed = move_toward(speed, 0.0, brake_force * 0.5 * delta)
 	
 	path_follow.progress += speed * delta
 	
+	_update_headlights()
 	_update_brake_light(back > 0 and speed > 0)
+
+func _update_headlights():
+	if not player_inside:
+		spot_front_right.visible = false
+		spot_front_left.visible = false
+		spot_back_right.visible = false
+		spot_back_left.visible = false
+		return
 	
-func _update_brake_light(_braking: bool):
-	pass
+	spot_front_right.visible = gear == 1
+	spot_front_left.visible = gear == 1
+	
+	spot_back_right.visible = gear == -1
+	spot_back_left.visible = gear == -1
+	
+func _update_brake_light(is_braking: bool):
+	var energy_normal := 3.0
+	var energy_braking := 6.0
+	
+	if gear >= 0:
+		back_back_right.visible = true
+		back_back_left.visible = true
+		back_front_right.visible = false
+		back_front_left.visible = false
+		back_back_right.light_energy = lerp(back_back_right.light_energy, energy_braking if is_braking else energy_normal, 0.1)
+		back_back_left.light_energy = lerp(back_back_left.light_energy, energy_braking if is_braking else energy_normal, 0.1)
+	else:
+		back_front_right.visible = true
+		back_front_left.visible = true
+		back_back_right.visible = false
+		back_back_left.visible = false
+		back_front_right.light_energy = lerp(back_front_right.light_energy, energy_braking if is_braking else energy_normal, 0.1)
+		back_front_left.light_energy = lerp(back_front_left.light_energy, energy_braking if is_braking else energy_normal, 0.1)
 	
 func enter_vehicle(player):
 	player_ref = player
